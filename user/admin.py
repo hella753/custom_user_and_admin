fa3d0a2004e2c2273f87657c0f93d13640c89089
hella753 <kristi.gaphrindashvili@gmail.com>
2024-10-17 18:39:16 +0400

    og admin user

--- user/admin.py
+++ user/admin.py
@@ -1,10 +1,6 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from user.models import User
 
 
-@admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    list_display = ('first_name', 'last_name', 'username', 'email', 'is_staff', 'is_superuser')
-    list_filter = ('is_staff', 'is_superuser')
-    search_fields = ('first_name', 'last_name', 'username', 'email')
-    list_per_page = 10
+admin.site.register(User)
